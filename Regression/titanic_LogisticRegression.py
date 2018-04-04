7e9e27b3204e0443c165dd8041513cd68ebac054
nimrod z <nimz911@gmail.com>
2018-04-04 12:13:27 +0300

    update

--- Regression/titanic_LogisticRegression.py
+++ Regression/titanic_LogisticRegression.py
@@ -5,14 +5,64 @@ Created on Tue Apr  3 11:58:10 2018
 
 kaggle titanic challenge
 """
-
 import pandas as pd
 import numpy as np
 #%%
+df = pd.read_csv('train.csv')
+df = df.fillna(df.mean())
+df = df[pd.notnull(df['Embarked'])] # Remove 2 'Embarked' NAN rows
+
+#%%
+df['Embarked_int'] = df['Embarked'].astype('category').cat.codes
+df['Sex_int'] = df['Sex'].astype('category').cat.codes
+X = df[['Age','Sex_int','SibSp','Pclass','Parch','Embarked_int','Fare']].values
+y = df['Survived'].values
+#%%
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+#%%
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+#%%
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(x_train, y_train)
+
+#%%
+# Applying Grid Search to find the best model and the best parameters
+from sklearn.model_selection import GridSearchCV
+#C = [0.047,0.048,0.049,0.05,0.051]
+C = np.arange(0.046,0.05,0.00001)
+penalty1 = ['l1']
+penalty2 = ['l2']
+multi_class = ['ovr']
+solver1 = ['liblinear']
+solver2 = ['lbfgs','sag','newton-cg']
+tol = [0.00001,0.00005,0.001]
+max_iter = [250,500,1000]
+parameters = [{'C': C, 'penalty':penalty1, 'solver':solver1, 'multi_class':multi_class, 'max_iter':max_iter, 'tol':tol},
+              {'C':C, 'penalty':penalty2,'solver':solver2, 'multi_class':multi_class, 'max_iter':max_iter, 'tol':tol}]
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10,
+                           n_jobs = 3)
+grid_search = grid_search.fit(x_train, y_train)
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
+
+#%%
 # Read data
 test = pd.read_csv('test.csv')
-train = pd.read_csv('train.csv').fillna(test.mean())
-
+train = pd.read_csv('train.csv')
+train = train.fillna(train.mean())
 
 #%%
 # Check for missing data in test features
@@ -49,48 +99,20 @@ x_test = sc.transform(x_test)
 #%%
 # Fitting Logistic Regression to the Training set
 from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
-classifier.fit(x_train, y_train)
-
-#%%
-# Applying Grid Search to find the best model and the best parameters
-from sklearn.model_selection import GridSearchCV
-#C = [0.047,0.048,0.049,0.05,0.051]
-C = np.arange(0.048,0.05,0.000001)
-penalty1 = ['l1']
-penalty2 = ['l2']
-multi_class = ['ovr']
-solver1 = ['liblinear']
-solver2 = ['lbfgs','sag','newton-cg']
-#tol = [0.0000000001,0.00005,0.0001,0.5,1,10,100]
-max_iter = [250,500,1000]
-parameters = [{'C': C, 'penalty':penalty1, 'solver':solver1, 'multi_class':multi_class, 'max_iter':max_iter},
-              {'C':C, 'penalty':penalty2,'solver':solver2, 'multi_class':multi_class, 'max_iter':max_iter}]
-grid_search = GridSearchCV(estimator = classifier,
-                           param_grid = parameters,
-                           scoring = 'accuracy',
-                           cv = 10,
-                           n_jobs = -1)
-grid_search = grid_search.fit(x_train, y_train)
-best_accuracy = grid_search.best_score_
-best_parameters = grid_search.best_params_
-
-#%%
-# Fitting Logistic Regression to the Training set
-from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(**grid_search.best_params_, random_state = 0)
 classifier.fit(x_train, y_train)
+
 
 #%%
 # Predicting the Test set results
 y_pred = classifier.predict(x_test)
 #%%
 
-predicion = pd.DataFrame({'PassengerId':test['PassengerId'], 'prediction':y_pred})
-predicion.to_csv('submission.csv')
+predicion = pd.DataFrame({'PassengerId':test['PassengerId'], 'Survived':y_pred},index=None)
+predicion.to_csv('submission.csv', index=False)
 #%%
 
-'''
+
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix, accuracy_score, r2_score, classification_report
 cm = confusion_matrix(y_test, y_pred)
@@ -99,6 +121,7 @@ r2 = r2_score(y_test, y_pred)
 classification_report = classification_report(y_test,y_pred,digits=3)
 print('accuracy: '+str(accuracy)+'\nr2: '+str(r2)+'\nclassification_report:\n\n'+classification_report)
 
+#%%
 import matplotlib.pyplot as plt
 def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Oranges):
     import itertools
@@ -124,6 +147,6 @@ fig, ax = plt.subplots()
 plot_confusion_matrix(cm)
 
 plt.show()
-'''
+
 #%%
 
